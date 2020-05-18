52a9890a789bc9c5f7fe2cb6e7e123142996316d
Minhaz Ahmed Chowdhury <65192028+MINHAZ1981@users.noreply.github.com>
2020-05-19 02:18:53 +0600

    function makeCacheMatrix created

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
+## here wrote a pair of functions, namely, "makeCacheMatrix" and "cacheSolve"
+## that cache the inverse of a matrix
+
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## cache its inverse for the input (which is an invertible square matrix)
 
+makeCacheMatrix <- function(x = matrix()) {
+    
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inverse) inv <<- inverse
+    getinv <- function() inv
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
